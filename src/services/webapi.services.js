645a66cf8a074a0916a3cd55bcfa6a5b70ae545c
Daniel Mendes <dmendesrbh@gmail.com>
2023-11-19 16:28:29 -0300

    exclusão evento

--- src/services/webapi.services.js
+++ src/services/webapi.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://forty-shoes-give.loca.lt';
+export const BASE_URL = 'https://sweet-forks-flow.loca.lt';
 
 const API = axios.create({
     baseURL: BASE_URL,
