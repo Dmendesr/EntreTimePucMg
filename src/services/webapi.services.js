40acef44779ae6fbba0f0a933440632d0ed6e67a
Daniel Mendes <dmendesrbh@gmail.com>
2023-11-23 14:42:01 -0300

    apagando id 4 terceiro evento

--- src/services/webapi.services.js
+++ src/services/webapi.services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://chilly-camels-hunt.loca.lt';
+export const BASE_URL = 'https://green-banks-rule.loca.lt';
 
 const API = axios.create({
     baseURL: BASE_URL,
